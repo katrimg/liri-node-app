d734f21d9a0f55f0f611c2bd4dbe6eae532572f4
Katri Morrison-Goulias <katrimorrison-goulias@Konstadinoss-MacBook-Air.local>
2018-07-12 21:21:16 -0700

    working twitter api

--- liri.js
+++ liri.js
@@ -2,24 +2,25 @@
 //read and set environment variables for the .env package
 require("dotenv").config();
 
-//access your keys information like so
-//var spotify = new Spotify(keys.spotify);
-
+var keys = require('./keys.js')
 var Twitter = require('twitter');
 
-var tweets = require("./key.js");
 //Add the code required to import the `keys.js` file and store it in a variable.
 var client = new Twitter({
-    consumer_key: 'process.env.T_CK',
-    consumer_secret: 'process.env.T_CS',
-    access_token_key: 'process.env.T_AT',
-    access_token_secret: 'process.env.T_TS'
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token_key: keys.twitter.access_token_key,
+    access_token_secret: keys.twitter.access_token_secret
 });
 
 var params = { screen_name: 'katritest', count: 20 };
 
 //first command: node liri.js my-tweets
+
+//if the argument is "my-tweets", then
 if (process.argv[2] === "my-tweets") {
+
+    //get statuses on user timeline & pass in the parameters
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             console.log("here are the tweets");
@@ -33,6 +34,10 @@ if (process.argv[2] === "my-tweets") {
 
 
 //second command: node liri.js spotify-this-song
+
+
 //third command: node liri.js movie-this
+
+
 //fourth command: node liri.js do-what-it-says
 
